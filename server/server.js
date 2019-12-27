7f33c3667a3ee8d88299c92f1378176d1a6df071
triazur <guillaume.cassin@triazur.com>
2019-12-27 20:03:47 +0100

    implement all API endpoints and create angular web app

--- server/server.js
+++ server/server.js
@@ -34,9 +34,9 @@ server.use((req, res, next) => {
 });
 server.use('/', router);
 
-const hostname = 'localhost';
+// const hostname = '192.168.1.20';
 const port = process.env.PORT || 5000;
 
-server.listen(port, hostname, () => {
-	console.log(`Server started at http://${hostname}:${port}`)
-});
+server.listen(port, () => {
+    console.log(`Server started at port:${port}`)
+  })
